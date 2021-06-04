0d0fb14d041015a55fa37c3e9b7de9317ce61ffd
Trumane12311 <Trumane12311@gmail.com>
2021-06-03 20:37:11 -0400

    Initial and Coding

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,28 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function createTable (data) {
+    tbody.html("");
+    data.forEach(( row ) => {
+        var datarow = tbody.append("tr");
+        Object.values(row).forEach(( value ) => {
+            var cell = datarow.append("td");
+            cell.text(value)
+        })
+    })
+}
+
+createTable(tableData);
+
+function searchTable () {
+    var date = d3.select("#datetime").property("value");
+    if (date)  {
+        var filterdata = tableData.filter(row => row.datetime === date);
+    } 
+    createTable(filterdata);
+}
+
+d3.selectAll("#filter-btn").on("click", searchTable);
