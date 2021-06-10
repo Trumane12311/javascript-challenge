71ad8cc6a27006be544946d235c621f4e0a13804
Trumane12311 <Trumane12311@gmail.com>
2021-06-09 23:28:26 -0400

    Updated filter variables

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,14 +7,14 @@ var tbody = d3.select("tbody");
 // begin function to create the table.
 function createTable (data) {
     tbody.html("");
-    data.forEach(( row ) => {
+    data.forEach(function(UFO) {
         var datarow = tbody.append("tr");
-        Object.values(row).forEach(( value ) => {
+        Object.entries(UFO).forEach(([key, value]) => {
             var cell = datarow.append("td");
             cell.text(value)
         })
     })
-}
+};
 // Call the createTable function to display data in the HTML body.
 createTable(tableData);
 
@@ -23,32 +23,31 @@ when the search date button is pressed */
 
 function searchTable () {
     //Create multi
-    var date = d3.select("#datetime").property("value");
-    var city = d3.select("#city").property("value").toLowerCase();
-    var state = d3.select("#state").property("value").toLowerCase();
-    var country = d3.select("#country").property("value").toLowerCase();
-    var shape = d3.select("#shape").property("value").toLowerCase();
-    if (date.length > 0)  {
-        var filterdata = tableData.filter(row => row.datetime === date);
+    var inputDate = d3.select("#datetime").property("value").toLowerCase();
+    var inputCity = d3.select("#city").property("value").toLowerCase();
+    var inputState = d3.select("#state").property("value").toLowerCase();
+    var inputCountry = d3.select("#country").property("value").toLowerCase();
+    var inputShape = d3.select("#shape").property("value").toLowerCase();
+    if (inputDate.length > 0)  {
+        var filterdata = tableData.filter(row => row.datetime === inputDate);
         console.log(filterdata);
-    }
-    if (city.length > 0)  {
-        var filterdata = tableData.filter(row => row.city === city);
-        console.log(filterdata);
-    } 
-    if (state.length > 0)  {
-        var filterdata = tableData.filter(row => row.state === state);
+        }
+    if (inputCity.length > 0)  {
+        var filterdata = tableData.filter(row => row.city === inputCity);
         console.log(filterdata);
     } 
-    if (country.length > 0)  {
-        var filterdata = tableData.filter(row => row.country === country);
+    if (inputState.length > 0)  {
+        var filterdata = tableData.filter(row => row.state === inputState);
         console.log(filterdata);
     } 
-    if (shape.length > 0)  {
-        var filterdata = tableData.filter(row => row.shape === shape);
+    if (inputCountry.length > 0)  {
+        var filterdata = tableData.filter(row => row.country === inputCountry);
         console.log(filterdata);
     } 
-    console.log(filterdata);
+    if (inputShape.length > 0)  {
+        var filterdata = tableData.filter(row => row.shape === inputShape);
+        console.log(filterdata);
+    } 
     createTable(filterdata);
 }
 // Call the function to run the filter when the search button is "clicked" by the user
