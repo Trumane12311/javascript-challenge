a0bb587ee41a1a71f0952c04df4615a878c0d42f
Trumane12311 <Trumane12311@gmail.com>
2021-06-09 23:40:16 -0400

    Updated varibles and testing combining filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -20,37 +20,60 @@ createTable(tableData);
 
 /* Create a function to filter the javascript file
 when the search date button is pressed */
+button.on("click", () => {
+    d3.event.preventDefault();
 
-function searchTable () {
-    //Create multi
-    var inputDate = d3.select("#datetime").property("value").toLowerCase();
-    var inputCity = d3.select("#city").property("value").toLowerCase();
+    var inputDate = d3.select("#datetime").property("value").trim();
+    var inputCity = d3.select("#city").property("value").toLowerCase().trim();
     var inputState = d3.select("#state").property("value").toLowerCase();
     var inputCountry = d3.select("#country").property("value").toLowerCase();
     var inputShape = d3.select("#shape").property("value").toLowerCase();
     if (inputDate.length > 0)  {
-        var filterdata = tableData.filter(row => row.datetime === inputDate);
+        var filterdata = tableData.filter(tableData => tableData.datetime === inputDate);
         console.log(filterdata);
         }
     if (inputCity.length > 0)  {
-        var filterdata = tableData.filter(row => row.city === inputCity);
+        var filterdata = tableData.filter(tableData => tableData.city === inputCity);
         console.log(filterdata);
     } 
     if (inputState.length > 0)  {
-        var filterdata = tableData.filter(row => row.state === inputState);
+        var filterdata = tableData.filter(tableData => tableData.state === inputState);
         console.log(filterdata);
     } 
     if (inputCountry.length > 0)  {
-        var filterdata = tableData.filter(row => row.country === inputCountry);
+        var filterdata = tableData.filter(tableData => tableData.country === inputCountry);
         console.log(filterdata);
     } 
     if (inputShape.length > 0)  {
-        var filterdata = tableData.filter(row => row.shape === inputShape);
+        var filterdata = tableData.filter(tableData => tableData.shape === inputShape);
         console.log(filterdata);
     } 
-    createTable(filterdata);
-}
-// Call the function to run the filter when the search button is "clicked" by the user
-d3.selectAll("#filter-btn").on("click", searchTable);
 
-createTable(tableData);
+})
+// Call the function to run the filter when the search button is "clicked" by the user
+$tbody.html("")
+
+let response = {
+    inputDate, inputCity, inputCombinedData
+}
+// Top if only works for filtering the date
+// Need to accommodate for combining multiple filters, needed to create a new var for it
+if(response.filterCombinedData.length !== 0) {
+    addData(filterCombinedData);
+}
+
+// else if(response.filterCity.length !== 0){
+//     addData(filterCity);
+// }
+    else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
+        addData(filterDate) || addData(filterCity);
+    }
+
+    // else if(response.filterCombinedDateState.length === 0 && ((response.filterDate.length !== 0 || response.filterState.length !== 0))) {
+    //     addData(filterDate) || addData(filterState);
+    // }
+
+    else {
+        $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+    }
+})
