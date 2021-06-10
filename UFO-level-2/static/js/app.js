12bc86751900bd27d29e4d6640125adb4993ae2c
Trumane12311 <Trumane12311@gmail.com>
2021-06-09 20:01:44 -0400

    Testing removal of case sensitivity

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -24,27 +24,27 @@ when the search date button is pressed */
 function searchTable () {
     //Create multi
     var date = d3.select("#datetime").property("value");
-    var city = d3.select("#city").property("value");
+    var city = d3.select("#city").property("value").toLowerCase();
     var state = d3.select("#state").property("value");
     var country = d3.select("#country").property("value");
     var shape = d3.select("#shape").property("value");
-    if (date.length)  {
+    if (date.length > 0)  {
         var filterdata = tableData.filter(row => row.datetime === date);
         console.log(filterdata);
     }
-    if (city)  {
+    if (city.length > 0)  {
         var filterdata = tableData.filter(row => row.city === city);
         console.log(filterdata);
     } 
-    if (state)  {
+    if (state.length > 0)  {
         var filterdata = tableData.filter(row => row.state === state);
         console.log(filterdata);
     } 
-    if (country)  {
+    if (country.length > 0)  {
         var filterdata = tableData.filter(row => row.country === country);
         console.log(filterdata);
     } 
-    if (shape)  {
+    if (shape.length > 0)  {
         var filterdata = tableData.filter(row => row.shape === shape);
         console.log(filterdata);
     } 
@@ -53,3 +53,5 @@ function searchTable () {
 }
 // Call the function to run the filter when the search button is "clicked" by the user
 d3.selectAll("#filter-btn").on("click", searchTable);
+
+createTable(tableData);
